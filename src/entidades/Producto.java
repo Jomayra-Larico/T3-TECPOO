babe6f4ff9869d8f170caf784f1ceaf188fd3ecc
Jomayra Larico <94720502+Jomayra-Larico@users.noreply.github.com>
2023-06-02 14:28:56 -0500

    Add files via upload

---
+++ src/entidades/Producto.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author Jomayra Larico
+ */
+public class Producto 
+    implements Imprime{
+    
+    private String codigo;
+    private String nombre;
+    private Double precio;
+    
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String mostrarContenido() {
+        return this.nombre + " s/ " + this.precio;
+    }
+    
+}
---
+++ src/tiendamiguelito/TiendaMiguelito.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendamiguelito;
+
+import pantallas.JFramePrincipal;
+
+/**
+ *
+ * @author Jomayra Larico
+ */
+public class TiendaMiguelito {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFramePrincipal principal = new JFramePrincipal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
